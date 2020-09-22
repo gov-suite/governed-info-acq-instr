72d4e5ba6b70b5c9fd4d77abfe04dd9c31c3d87d
Shahid N. Shah <shahid@shah.org>
2020-09-21 23:25:59 -0400

    Support retrieving lform.ts from GitHub or local fs

--- core/deps.ts
+++ core/deps.ts
@@ -1,4 +1,4 @@
-export * as fs from "https://deno.land/std@0.69.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.69.0/path/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.0.2/mod.ts";
-export * as typedDataGen from "https://denopkg.com/shah/tsd-typed-data-gen@v0.5.4/mod.ts";
+export * as fs from "https://deno.land/std@v0.70.0/fs/mod.ts";
+export * as path from "https://deno.land/std@v0.70.0/path/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.0.8/mod.ts";
+export * as typedDataGen from "https://denopkg.com/shah/tsd-typed-data-gen@v1.0.5/mod.ts";
