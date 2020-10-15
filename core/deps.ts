6dff8ebe17a841a0510ee19cde85d4a1d36c0e65
Shahid N. Shah <shahid@shah.org>
2020-10-14 22:07:53 -0400

    fix: update dependencies

--- core/deps.ts
+++ core/deps.ts
@@ -1,3 +1,3 @@
 export * as fs from "https://deno.land/std@v0.73.0/fs/mod.ts";
 export * as path from "https://deno.land/std@v0.73.0/path/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.3/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.9/mod.ts";
