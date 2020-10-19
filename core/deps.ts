dbdd0295ae81b3452e1a895eb9f6d5be21a92e53
Shahid N. Shah <shahid@shah.org>
2020-10-19 10:09:15 -0400

    fix: remove Deno std@v from dependencies

--- core/deps.ts
+++ core/deps.ts
@@ -1,3 +1,3 @@
-export * as fs from "https://deno.land/std@v0.73.0/fs/mod.ts";
-export * as path from "https://deno.land/std@v0.73.0/path/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.9/mod.ts";
+export * as fs from "https://deno.land/std@0.74.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.74.0/path/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v2.0.2/mod.ts";
