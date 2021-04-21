c8d49317c663cab7fb7889b98c43609ad1c03489
geo_vl <geo@citrusinformatics.com>
2021-04-21 15:42:08 +0530

    update: update docopt update for deno 1.9.0

--- core/deps.ts
+++ core/deps.ts
@@ -1,4 +1,4 @@
-export * as fs from "https://deno.land/std@0.80.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v2.2.2/mod.ts";
+export * as fs from "https://deno.land/std@0.94.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.94.0/path/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v2.2.4/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
